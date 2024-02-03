4f384242d7ebc53b994c87c74f5fd8f9846eeaeb
MaciejSikora <maciek.sikora@gmail.com>
2024-02-03 16:44:47 +0100

    first commit

---
+++ build-Image_Compressor-Desktop_Qt_6_7_0_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Image_Compressor_FILE F:/Maciek/Program/ProjektOLX/ImageCompressor/build-Image_Compressor-Desktop_Qt_6_7_0_MSVC2019_64bit-Debug/Image_Compressor.exe)
+set(__QT_DEPLOY_TARGET_Image_Compressor_RUNTIME_DLLS C:/Qt/6.7.0/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.0/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.0/msvc2019_64/bin/Qt6Cored.dll)
